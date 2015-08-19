f6737c3449ad75a80aa62c511d5894c9e5dcca61
Austin Herring <awh44@drexel.edu>
2015-08-19 18:07:07 -0400

    Made load_data.bash immune to changes in schema

--- setup/load_data.bash
+++ setup/load_data.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 DATABASE="courses.db"
-TMPFILE="tmpfile"
 CREATEPOSTGRES="sql/create_postgres.sql"
+export DATABASE
 
 dump_filter()
 {
@@ -10,36 +10,19 @@ dump_filter()
 	sqlite3 $DATABASE ".dump $1" | grep 'INSERT' | sed 's/INSERT INTO "\([^"]\+\)"/INSERT INTO \1/'
 }
 
-dump_filter_convert()
+get_tables()
 {
-	#The sqlite database currently uses integers in some places it should be using text, so some
-	#leading zeros have been removed. Look for the one and two leading zero cases and fix them
-	dump_filter "$1" | sed 's/,\([0-9][0-9]\),/,'\''0\1'\'',/' | sed 's/,\([0-9]\),/,'\''00\1'\'',/'
+	#Use the create table statements for postgres to get the table names. This whole script assumes that
+	#the sqlite and postgres tables have the same names, so could use either the create script or a
+	#.dump of the database, so use the script for efficiency's sake
+	grep 'CREATE TABLE' $CREATEPOSTGRES | sed 's/CREATE TABLE \([a-zA-Z_]*\)/\1/'
 }
 
-dump_filter_write()
-{
-	dump_filter "$1" >> $TMPFILE
-}
-
-dump_filter_convert_write()
-{
-	dump_filter_convert "$1" >> $TMPFILE
-}
+export -f dump_filter
+export -f get_tables
 
 if [[ $# -ne 3 ]] ; then
 	echo Please include hostname, username, and database name to which to connect.
 else
-	cat $CREATEPOSTGRES > $TMPFILE
-	dump_filter_write Subjects
-	dump_filter_convert_write Courses_Have
-	dump_filter_write Terms
-	dump_filter_convert_write Course_Offered_In_Term
-	dump_filter_write Instructors
-	dump_filter_write Campuses
-	dump_filter_write Sections
-	dump_filter_write TimeBlocks
-	dump_filter_write Meets_At
-	psql -h $1 -U $2 $3 < $TMPFILE > /dev/null
-	rm $TMPFILE
+	cat $CREATEPOSTGRES <(get_tables | xargs -n 1 -i bash -c 'dump_filter "$@"' _ {}) | psql -h $1 -U $2 $3 > /dev/null
 fi
